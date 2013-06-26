2b17fb3d3a4c237e210ef69e13abdcc1f20f23f3
Flávio Juvenal <flaviojuvenal@gmail.com>
2013-06-26 13:39:42 -0300

    Show markers only if logged and show only markers for displayed events

--- client/views/eventsMap.js
+++ client/views/eventsMap.js
@@ -6,6 +6,7 @@ Template.eventsMap.rendered = function() {
 
   var map = new google.maps.Map(document.getElementById("events-map-canvas"), mapOptions);
   var infowindow = new google.maps.InfoWindow({ content: '...' });
+  var markersArray = [];
   var geolocation = Geolocation.get();
 
   if (geolocation && geolocation.coords && geolocation.coords.latitude && geolocation.coords.longitude) {
@@ -19,6 +20,7 @@ Template.eventsMap.rendered = function() {
       icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
     });
     marker.setMap(map);
+    markersArray.push(marker);
 
     google.maps.event.addListener(marker, 'click', function() {
       infowindow.content = 'Você está aqui!';
@@ -28,17 +30,30 @@ Template.eventsMap.rendered = function() {
     map.setCenter(new google.maps.LatLng('-15.779039', '-47.928071'));  // Brasília LatLng
     map.setZoom(3);
   }
-
   Session.set('eventsMapRendered', true);
+
+  var clearMarkers = function () {
+    // based on: https://developers.google.com/maps/documentation/javascript/overlays#RemovingOverlays
+    if (markersArray) {
+      for (var i in markersArray) {
+        markersArray[i].setMap(null);
+      }
+      markersArray.length = 0;
+    }
+  };
 
   Deps.autorun(function () {
     var isRendered = Session.get('eventsMapRendered');
-    if (isRendered) {
+    var isLogged = Facebook.getAccessToken();
+
+    if (isRendered && isLogged) {
       var todayKey = SelectedDate.getAsKey();
       if (todayKey) {
         var tomorrowKey = SelectedDate.getTomorrowAsKey();
         var todayEvents = Facebook.getEventsByDate(todayKey).fetch();
         var tomorrowEvents = Facebook.getEventsByDate(tomorrowKey).fetch();
+
+        clearMarkers();
         _(todayEvents.concat(tomorrowEvents)).each(function (ev) {
           if (ev.venue && ev.venue.latitude && ev.venue.longitude) {
             var marker = new google.maps.Marker({
@@ -47,6 +62,7 @@ Template.eventsMap.rendered = function() {
               eventId: ev.id
             });
             marker.setMap(map);
+            markersArray.push(marker);
 
             google.maps.event.addListener(marker, 'click', function() {
               infowindow.content = Template.eventInfoWindow(ev);
