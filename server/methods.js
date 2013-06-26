6ff3cf9f9f26cfa7be53eba3252da2d2f75266fd
Flávio Juvenal <flaviojuvenal@gmail.com>
2013-06-25 23:58:01 -0300

    Fixing timezone parsing in server

--- server/methods.js
+++ server/methods.js
@@ -1,5 +1,12 @@
 var fbDateFormats = ["YYYY-MM-DDThh:mm:ssZZ", "YYYY-MM-DD", "YYYY-MM-DDThh:mm:ss"];
-var protestRegex = /protesto|protesta|manifestação|reforma|movimento |luta | ato |greve|paralização|boicote|revolução|revolta|ocupa|marcha|impeachment|vem pra rua|vem para as ruas|massa crítica|direitos urbanos|primavera brasileira|#vemprarua|#changebrazil|#forafeliciano|pec37|pec 37|pec33|pec 33|cura gay|curagay|anonymous/i
+var fbDateToDate = function (fbDate) {
+  var fbDateMoment = moment(fbDate, fbDateFormats);
+  if (fbDate.length == 10 || fbDate.length == 19 /* fbDateFormats[1] or [2] */) {  // if there is no timezone...
+    fbDateMoment.add('hours', 3);  // use GMT-3.
+  }
+  return fbDateMoment.toDate();
+};
+var protestRegex = /protesto|protesta|manifestação|reforma|movimento |luta | ato |greve|paralização|boicote|revolução|revolta|ocupa|marcha|impeachment|vem pra rua|vem para as ruas|massa crítica|direitos urbanos|primavera brasileira|#vemprarua|#changebrazil|#forafeliciano|pec37|pec 37|pec33|pec 33|cura gay|curagay|anonymous/i;
 
 Meteor.methods({
   insertEvents: function (events) {
@@ -7,9 +14,9 @@ Meteor.methods({
     _.each(events, function (ev) {
       if (ev.name.search(protestRegex) !== -1) {  // if is a protest event
         // convert start_time and end_time to Date
-        ev.start_time = moment(ev.start_time, fbDateFormats).toDate();
+        ev.start_time = fbDateToDate(ev.start_time);
         if (ev.end_time) {
-          ev.end_time = moment(ev.end_time, fbDateFormats).toDate();
+          ev.end_time = fbDateToDate(ev.end_time);
         }
 
         // create lnglat field
