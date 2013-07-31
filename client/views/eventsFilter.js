78153c65f1017244065133359ee9f3b9c8f02e71
Flávio Juvenal <flaviojuvenal@gmail.com>
2013-07-31 20:20:22 -0300

    Refactoring events show all filters to a new template

--- client/views/eventsFilter.js
+++ client/views/eventsFilter.js
@@ -1,3 +1,8 @@
+/*
+ * Events filter click events.
+ * Clicking in #show-near will show only nearby geolocation events.
+ * Clicking in #show-all will show all events.
+ */
 Template.main.events({
   "click #show-near": function () {
     Geolocation.setShowAll(false);
