f620945f496c68bc10afba7d272c5660e8c13c0b
Flávio Juvenal <flaviojuvenal@gmail.com>
2013-07-31 21:29:50 -0300

    Changing run_rests messages

--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-echo "Starting servers..."
+echo "Starting test servers..."
 mrt >/dev/null &
 sleep 3  # wait for mrt start
 METEOR_MOCHA_TEST_DIRS="$(pwd)/client/tests/" MONGO_URL="mongodb://127.0.0.1:3002/test_$(date +%s%N)" mrt -p 8000 >/dev/null &
 sleep 3  # wait for mrt start
 
 echo "Ready to test! Go to http://localhost:8000/test"
-echo 'Use Ctrl+C to stop'
+echo "NOTICE: Tests should be executed only once. To test again, please restart servers."
+echo "Use Ctrl+C to stop test servers."
 trap 'fuser 8000/tcp -k && fuser 3000/tcp -k' INT  # kill running servers on SIGINT
 tail -f /dev/null  # wait forever for SIGINT
